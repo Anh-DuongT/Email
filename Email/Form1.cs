328d1e0edc218a6968fcf7d54a2219042d1b3153
DELL 5448 <DELL 5448@DELL>
2018-05-08 14:51:55 +0700

    email

--- Email/Form1.cs
+++ Email/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
 
 namespace Email
 {
@@ -15,6 +18,19 @@ namespace Email
         public Form1()
         {
             InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GuiEmail(textBox1.Text, textBox3.Text, textBox4.Text, textBox5.Text);
+        }
+        public void GuiEmail(string from, string to, string subject, string message)
+        {
+            MailMessage mess = new MailMessage(from, to, subject, message);
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential(textBox1.Text, textBox2.Text);
+            client.Send(mess);
+            MessageBox.Show("Gui thanh cong");
         }
     }
 }
